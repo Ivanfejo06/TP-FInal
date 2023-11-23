ddd4cc22cb4146f74f76c716daf459f73b9f99c6
MARCOS MARTINEZ <delfrancochili@gmail.com>
2023-11-23 09:04:50 -0300

    Segui avanzando con las views

--- TP_Final/Controllers/HomeController.cs
+++ TP_Final/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ public class HomeController : Controller
     
      ViewBag.ListarForos = BD.MostrarForosPrincipal(cantidad);
 
-        return View("Registro");
+        return View("UpdatearDatos");
        
     }
 
@@ -58,6 +58,12 @@ public class HomeController : Controller
     {
         ViewBag.Usuario = BD.MostrarPerfil(IdUsuario);
         return View("Configuracion");
+    }
+
+    public IActionResult UpdateDataUsuario(Usuario Us)
+    {
+        ViewBag.Usuario = Us;
+        return View("UpdatearDatos");
     }
 
    
