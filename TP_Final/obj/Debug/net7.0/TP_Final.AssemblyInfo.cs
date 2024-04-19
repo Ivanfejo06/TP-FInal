ecd04dba03815946d3f420d2a889ff2b989f19c1
IVAN JOAQUIN <MY_NAME@example.comivanfejo06@gmail.com>
2024-04-19 10:28:31 -0300

    terminado

--- TP_Final/obj/Debug/net7.0/TP_Final.AssemblyInfo.cs
+++ TP_Final/obj/Debug/net7.0/TP_Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff919ffb49596396fcf6194eb1b2c29930571d1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a016027b5d651a0a54662042e5536a0e698e7c4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
